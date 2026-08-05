74e57bb0224442dab9013ddbb776ca003f71c98c
weihanyau <weihanyau@gmail.com>
2026-08-05 23:08:13 +0800

    done statick response

--- pwn.college/computing-101/building-a-web-server/static-response.s
+++ pwn.college/computing-101/building-a-web-server/static-response.s
@@ -33,7 +33,30 @@ _start:
 	syscall
 	mov r13, rax # accepted fd
 
+	// read request
+	sub rsp, 1024
+	mov rax, 0
+	mov rdi, r13
+	mov rsi, rsp
+	mov rdx, 1024
+	syscall
+
+	// response to request
+	mov rax, 1
+	mov rdi, r13
+	lea rsi, [rip+response]
+	mov rdx, 19
+	syscall
+
+	// close accepted fd
+	mov rax, 3
+	mov rdi, r13
+	syscall
+
 	// exit
 	mov rax, 60
 	mov rdi, 0
 	syscall
+
+response:
+	.asciz "HTTP/1.0 200 OK\r\n\r\n"
