ba0a619ccecc2a49f37f1cb6088f0b00385d6c34
weihanyau <weihanyau@gmail.com>
2026-08-05 21:37:23 +0800

    done listen

--- pwn.college/computing-101/building-a-web-server/bind.s
+++ pwn.college/computing-101/building-a-web-server/bind.s
@@ -8,14 +8,21 @@ _start:
 	mov rsi, 1
 	mov rdx, 0
 	syscall
+	mov r12, rax # sockfd
 
 	// bind
-	mov rdi, rax
+	mov rdi, r12
 	mov rax, 49
 	sub rsp, 16
 	mov qword ptr [rsp], 0x00000050000002
 	mov rsi, rsp
 	mov rdx, 16
+	syscall
+
+	// listen
+	mov rax, 50
+	mov rdi, r12
+	mov rsi, 0
 	syscall
 
 	// exit
